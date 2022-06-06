90e00f41269741b0b36a6b98a07cba5e311b903a
Jassil lou Segarino <102401385+jassillou07@users.noreply.github.com>
2022-06-06 10:47:23 +0800

    Add files via upload

---
+++ Activity 2.0 Segarino (1).c
@@ -0,0 +1,25 @@
+/**
+ * C program that calculate the total and average of five subjects
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    float history, phy, nstp, math, comp; 
+    float total, average, percentage;
+
+    /* Input marks of all five subjects */
+    printf("Enter marks of five grades: \n");
+    scanf("%f%f%f%f%f", & history, &phy, &nstp, &math, &comp);
+
+    /* Calculate total, average and percentage */
+    total = history + phy + nstp + math + comp;
+    average = total / 5.0;
+
+    /* Print all results */
+    printf("Total marks = %.2f\n", total);
+    printf("Average marks = %.2f\n", average);
+
+    return 0;
+}
